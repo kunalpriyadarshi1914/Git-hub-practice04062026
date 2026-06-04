57754cce180f45ac7e6aa2e3be102d25acc26d2f
kunal <kp4u2011@gmail.com>
2026-06-04 18:14:02 +0530

    modified provider

--- provider.tf
+++ provider.tf
@@ -1,11 +1,14 @@
-terraform{
-required_provider{
-azurerm={
-source="hashicorp/azurerm"
-version="4.74.0"
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=4.1.0"
+    }
+  }
 }
-}
-}
-provider"azurerm"{
-features{}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  resource_provider_registrations = "none" # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
+  features {}
 }
